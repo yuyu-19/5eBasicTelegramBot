01504d1743c58f383e499ace4a802898e8cec271
Yuri Cinesi Ferrari <yuri@cinesi.dev>
2022-11-18 12:28:45 +0100

    Misc commit

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
-import telegram
 import asyncio
+import json
+import logging
+import commandHandlers
+from telegram.ext import Updater, Dispatcher
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 
-# token: 5696001611:AAGeYWR8lY4NxkJgzC4m6mnj62PjEEQQ1Y4
+def registerHandlers(dispatcher: Dispatcher):
 
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), commandHandlers.messageHandler_echo)
+    dispatcher.add_handler(echo_handler)
 
-async def main():
-    bot = telegram.Bot("5696001611:AAGeYWR8lY4NxkJgzC4m6mnj62PjEEQQ1Y4")
-    async with bot:
-        print(await bot.get_me())
+    # Register all commandHandlers listed in the file
+    handlerPrefix = "commandHandler_"
+
+    method_list = [func for func in dir(commandHandlers) if callable(getattr(commandHandlers, func)) and func.startswith(handlerPrefix)]
+    for handlerName in method_list:
+        dispatcher.add_handler(CommandHandler(handlerName[len(handlerPrefix):len(handlerName)], getattr(commandHandlers, handlerName)))
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    # Logging config
+    with open("config.json") as f:
+        configData = json.load(f)
+
+
+    logLevel = logging.getLevelName(configData["logLevel"])
+    if type(logLevel) != int:
+        print("Invalid logLevel defined in config. Defaulting to ERROR.")
+        logLevel = 40
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logLevel
+    )
+
+    aps_logger = logging.getLogger('apscheduler')
+    aps_logger.setLevel(logging.WARNING)
+
+
+
+    updater = Updater(token=configData["token"], use_context=True)
+    registerHandlers(updater.dispatcher)
+
+    updater.start_polling()
 
